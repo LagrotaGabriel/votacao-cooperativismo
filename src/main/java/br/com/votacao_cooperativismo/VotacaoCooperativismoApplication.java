31d0bb017f289e7b3660db07adb4e23392f12069
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2024-11-13 00:20:50 -0300

    [MAIN] feat: Primeiro commit do projeto

---
+++ src/main/java/br/com/votacao_cooperativismo/VotacaoCooperativismoApplication.java
@@ -0,0 +1,13 @@
+package br.com.votacao_cooperativismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoCooperativismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoCooperativismoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/votacao_cooperativismo/VotacaoCooperativismoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.votacao_cooperativismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoCooperativismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
