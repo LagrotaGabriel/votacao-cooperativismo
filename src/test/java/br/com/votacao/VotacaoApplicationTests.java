6571562c37b4c3fda2374b2eb8a72fb11636aa21
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2024-11-13 00:24:55 -0300

    [DEVELOP] refac: Nome da classe principal da aplicação alterada de VotacaoCooperativismoApplication para VotacaoApplication

--- src/test/java/br/com/votacao/VotacaoApplicationTests.java
+++ src/test/java/br/com/votacao/VotacaoApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.votacao_cooperativismo;
+package br.com.votacao;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VotacaoCooperativismoApplicationTests {
+class VotacaoApplicationTests {
 
 	@Test
 	void contextLoads() {
