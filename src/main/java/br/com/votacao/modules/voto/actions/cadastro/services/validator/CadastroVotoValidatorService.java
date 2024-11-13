6fbdf0d78ad0a2255933ca61186bf1593e1613b9
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2024-11-13 17:07:48 -0300

    [FEAT-13] feat: Criados validadores do cadastro de votos

---
+++ src/main/java/br/com/votacao/modules/voto/actions/cadastro/services/validator/CadastroVotoValidatorService.java
@@ -0,0 +1,25 @@
+package br.com.votacao.modules.voto.actions.cadastro.services.validator;
+
+import br.com.votacao.modules.pauta.entity.PautaEntity;
+import br.com.votacao.modules.voto.entity.VotoEntity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface CadastroVotoValidatorService {
+
+    void realizaValidacoesDoCadastroDeVoto(
+      PautaEntity pauta,
+      UUID idAssociado
+    );
+
+    void validaSeAssociadoJaVotouNaPautaSelecionada(
+            List<VotoEntity> votosRealizadosNaPauta,
+            UUID idAssociado
+    );
+
+    void validaSePautaJaExpirou(
+            LocalDateTime dataHoraExpiracaoPauta
+    );
+}
