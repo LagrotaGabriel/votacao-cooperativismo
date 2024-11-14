40c8258429a4782adae5ca2ad2e52db5e520d749
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2024-11-14 10:41:34 -0300

    [DEVELOP] refac: Body de retorno da obtenção paginada de pautas não retorna mais os votos realizados em cada pauta. Para visualizar os votos das pautas de forma detalhada deverá ser utilizado o endpoint de consulta por ID

--- src/main/java/br/com/votacao/modules/pauta/actions/obtencao/paginada/models/ItemPautaPageResponse.java
+++ src/main/java/br/com/votacao/modules/pauta/actions/obtencao/paginada/models/ItemPautaPageResponse.java
@@ -1,16 +1,18 @@
 package br.com.votacao.modules.pauta.actions.obtencao.paginada.models;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.UUID;
 
 public record ItemPautaPageResponse(
 
+        UUID id,
         String titulo,
         String descricao,
         String status,
+        Integer quantidadeDeVotos,
         Integer quantidadeVotosSim,
         Integer quantidadesVotosNao,
         LocalDateTime dataHoraCriacao,
-        LocalDateTime dataHoraExpiracao,
-        List<VotosPautaPageResponse> votos
-) { }
+        LocalDateTime dataHoraExpiracao
+) {
+}
